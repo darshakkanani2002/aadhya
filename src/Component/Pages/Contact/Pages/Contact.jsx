a5852bb53d8ffcbfab6a208ef304921a8520edcf
darshakkanani2002 <kdarshak2910@gmail.com>
2024-09-26 17:12:04 +0530

    change in class attribute and put this new attribute className

--- src/Component/Pages/Contact/Pages/Contact.jsx
+++ src/Component/Pages/Contact/Pages/Contact.jsx
@@ -6,8 +6,8 @@ export default function Contact() {
         <div>
             <div className='padding-top-89'>
                 <div className='contact-hero'>
-                    <div class="container">
-                        <div class="contact-hero-content">
+                    <div className="container">
+                        <div className="contact-hero-content">
                             <div>
                                 <h1>Contact Us</h1>
                                 <p>We are here to help you with any queries and questions you may have. Get familiar with us and kick-start with our services.</p>
@@ -20,7 +20,7 @@ export default function Contact() {
             <div>
                 <div className='container'>
                     <div className='contact-form-back my-5 p-lg-5 p-md-5 p-sm-5 p-4'>
-                        <div className='row'>
+                        <div className='row align-items-center'>
                             <div className='col-lg-6'>
                                 <div className='contact-form-heading'>
                                     <h2>Get in <span>Touch</span></h2>
@@ -28,38 +28,38 @@ export default function Contact() {
                                         vel, ornare
                                         non id blandit netus.</p>
                                 </div>
-                                <form action="">
+                                <form>
                                     <div className='row'>
                                         <div className='col-lg-12'>
                                             <div>
-                                                <div class="form-floating my-3">
-                                                    <input type="text" class="form-control contact-us-input" id="floatingInput" placeholder="Name" required="" />
-                                                    <label for="floatingInput" class="contact-us-lable">Name <span className='text-danger'>*</span></label>
+                                                <div className="form-floating my-3">
+                                                    <input type="text" className="form-control contact-us-input" id="floatingInput" placeholder="Name" required="" />
+                                                    <label for="floatingInput" className="contact-us-lable">Name <span className='text-danger'>*</span></label>
                                                 </div>
                                             </div>
                                         </div>
                                         <div className='col-lg-12'>
                                             <div>
-                                                <div class="form-floating mb-3">
-                                                    <input type="email" class="form-control contact-us-input" id="floatingInput" placeholder="Email" />
-                                                    <label for="floatingInput" class="contact-us-lable">Email</label>
+                                                <div className="form-floating mb-3">
+                                                    <input type="email" className="form-control contact-us-input" id="floatingInput" placeholder="Email" />
+                                                    <label for="floatingInput" className="contact-us-lable">Email</label>
                                                 </div>
                                             </div>
                                         </div>
                                         <div className="col-lg-12">
                                             <div>
-                                                <div class="form-floating mb-3">
-                                                    <input type="text" class="form-control contact-us-input" id="floatingInput" placeholder="Phone number" required="" />
-                                                    <label for="floatingInput" class="contact-us-lable">Phone number
+                                                <div className="form-floating mb-3">
+                                                    <input type="text" className="form-control contact-us-input" id="floatingInput" placeholder="Phone number" required="" />
+                                                    <label for="floatingInput" className="contact-us-lable">Phone number
                                                         <span className='text-danger'>*</span></label>
                                                 </div>
                                             </div>
                                         </div>
                                         <div className='col-lg-12'>
                                             <div>
-                                                <div class="form-floating mb-3">
-                                                    <input type="text" class="form-control contact-us-input" id="floatingInput" placeholder="How did you find us?" />
-                                                    <label for="floatingInput" class="contact-us-lable">How did you find us?
+                                                <div className="form-floating mb-3">
+                                                    <input type="text" className="form-control contact-us-input" id="floatingInput" placeholder="How did you find us?" />
+                                                    <label for="floatingInput" className="contact-us-lable">How did you find us?
                                                     </label>
                                                 </div>
                                             </div>
@@ -67,36 +67,36 @@ export default function Contact() {
 
                                         <div className='col-lg-12'>
                                             <div>
-                                                <button class="contact-us-send-btn">SEND</button>
+                                                <button className="contact-us-send-btn">SEND</button>
                                             </div>
                                         </div>
                                     </div>
                                 </form>
 
-                                <div className='my-5'>
+                                <div className='mt-5'>
                                     <div className='row justify-content-center'>
-                                        <div className='col-lg-6 mb-3'>
+                                        <div className='col-lg-6 mb-lg-0 mb-md-3 mb-3'>
                                             <div className='d-flex align-items-center justify-content-lg-center justify-content-center'>
                                                 <div>
                                                     <img src="/images/ic_contect_us_phone_01.svg" alt="ic_contect_us_phone_01" className='img-fluid contact-img' />
                                                 </div>
                                                 <div className='contact-img-contant'>
                                                     <div className='ps-3'>
-                                                        <h6 class="mb-0">PHONE</h6>
-                                                        <p class="mb-0">03 5432 1234</p>
+                                                        <h6 className="mb-0">PHONE</h6>
+                                                        <p className="mb-0">03 5432 1234</p>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className='col-lg-6 mb-3'>
+                                        <div className='col-lg-6 mb-lg-0 mb-md-3 mb-3'>
                                             <div className='d-flex align-items-center justify-content-lg-center justify-content-center'>
                                                 <div>
                                                     <img src="/images/ic_contact_us_mail_03.svg" alt="ic_contact_us_mail_03" className='contact-img' />
                                                 </div>
                                                 <div className='contact-img-contant'>
                                                     <div className='ps-3'>
-                                                        <h6 class="mb-0">Email</h6>
-                                                        <p class="mb-0">info@marcc.com.au</p>
+                                                        <h6 className="mb-0">Email</h6>
+                                                        <p className="mb-0">info@marcc.com.au</p>
                                                     </div>
                                                 </div>
                                             </div>
