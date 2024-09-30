e5118b981b7da7e6916a29acc31dd6b4161c9015
darshakkanani2002 <kdarshak2910@gmail.com>
2024-09-30 10:53:19 +0530

    remove target

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -29,26 +29,26 @@ export default function Navbar() {
                             <div className="offcanvas-body">
                                 <ul className="navbar-nav m-auto mb-2 mt-0 mb-lg-0">
                                     <li className="nav-item d-flex align-items-center">
-                                        <Link to='/' className={`nav-link ${activeLink === '/' ? 'active' : ''}`} onClick={() => setActiveLink('/')} aria-current="page" data-section="home" target='_top'>Home</Link>
+                                        <Link to='/' className={`nav-link ${activeLink === '/' ? 'active' : ''}`} onClick={() => setActiveLink('/')} aria-current="page" data-section="home">Home</Link>
                                     </li>
                                     <li className="nav-item dropdown d-flex align-items-center">
                                         <Link to='/app' className={`nav-link ${activeLink === '/app' ? 'active' : ''}`} onClick={() => setActiveLink('/app')} id="servicesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                             Services
                                         </Link>
                                         <ul className="dropdown-menu nav-sub-menu py-0" aria-labelledby="servicesDropdown">
-                                            <li><Link to='/app' className="dropdown-item" target='_top'>App Development</Link></li>
-                                            <li><Link to='/game' className="dropdown-item" target='_top'>Game Development</Link></li>
-                                            <li><Link to='/web' className="dropdown-item" target='_top'>Web Development</Link></li>
+                                            <li><Link to='/app' className="dropdown-item">App Development</Link></li>
+                                            <li><Link to='/game' className="dropdown-item">Game Development</Link></li>
+                                            <li><Link to='/web' className="dropdown-item">Web Development</Link></li>
                                         </ul>
                                     </li>
                                     <li className="nav-item dropdown d-flex align-items-center">
-                                        <Link to='/technology' className={`nav-link ${activeLink === '/technology' ? 'active' : ''}`} onClick={() => setActiveLink('/technology')} target='_top'>Technologies</Link>
+                                        <Link to='/technology' className={`nav-link ${activeLink === '/technology' ? 'active' : ''}`} onClick={() => setActiveLink('/technology')} >Technologies</Link>
                                     </li>
                                     <li className="nav-item d-flex align-items-center">
-                                        <Link to='/portfolio' className={`nav-link ${activeLink === '/portfolio' ? 'active' : ''}`} onClick={() => setActiveLink('/portfolio')} data-section="blog" target='_top'>Portfolio</Link>
+                                        <Link to='/portfolio' className={`nav-link ${activeLink === '/portfolio' ? 'active' : ''}`} onClick={() => setActiveLink('/portfolio')} data-section="blog">Portfolio</Link>
                                     </li>
                                     <li className="nav-item d-flex align-items-center">
-                                        <Link to='/contact' className={`nav-link text-nowrap ${activeLink === '/contact' ? 'active' : ''}`} onClick={() => setActiveLink('/contact')} data-section="contact" target='_top'>Contact Us</Link>
+                                        <Link to='/contact' className={`nav-link text-nowrap ${activeLink === '/contact' ? 'active' : ''}`} onClick={() => setActiveLink('/contact')} data-section="contact">Contact Us</Link>
                                     </li>
                                 </ul>
                                 <div className="p-1 nav-contact-button mt-3 mt-lg-0" role="search">
